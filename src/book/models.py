b95a43835ffbdaacc8603f10817aa4ebd3b03c6a
FaNaTiK <Fan@fan.ru>
2024-07-29 12:00:45 +0800

    model

--- src/book/models.py
+++ src/book/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Users(models.Model):
+    pass
+
+
+class Books(models.Model):
+    autor = models.ForeignKey('Users')
