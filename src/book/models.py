9da555cb1e89f91d80a5c299feeb11f7152f8c1a
scorp <scorp@scorp.com>
2024-07-29 13:58:46 +0800

    create app user

--- src/book/models.py
+++ src/book/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 
-from django.contrib.auth.models import User
-
-
-class Users(models.Model):
-    pass
-
 
 class Books(models.Model):
-    autor = models.ForeignKey('Users')
+    autor = models.ForeignKey("Users")
---
+++ src/user/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbsractUser
+
+
+class Users(AbsractUser):
+    pass
+
+
+# Create your models here.
